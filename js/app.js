129f02ffcc1951c0a23fc237d40946e68660f1c3
Victor Wong <victor.wong1901@gmail.com>
2019-11-05 01:35:42 -0500

    html/css/js update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 // Accordion Logic, checks for active class on load and toggles the class accordingly
-var accordion = document.getElementsByClassName("module--accordion");
+var accordion = document.getElementsByClassName("module--top");
 
 for (var i = 0; i < accordion.length; i++) {
   // Checks on page load for any accordion element that has the active class to open
-  if (accordion[i].classList.contains('module--accordion__active')) {
-    accordion[i].parentNode.nextElementSibling.style.maxHeight = accordion[i].parentNode.nextElementSibling.scrollHeight + 'px';
+  if (accordion[i].children[1].classList.contains('module--accordion__active')) {
+    accordion[i].nextElementSibling.style.maxHeight = accordion[i].nextElementSibling.scrollHeight + 'px';
   }
   // Assign click event handler to each accordion
   accordion[i].addEventListener("click", function() {
-    this.classList.toggle("module--accordion__active");
-    var module = this.parentNode.nextElementSibling;
+    this.children[1].classList.toggle("module--accordion__active");
+    var module = this.nextElementSibling;
     // Checks on click if element is at maxHeight or not. If true, accordion will close.  If false, accordion will open.
     if (module.style.maxHeight) {
       module.style.maxHeight = null;
